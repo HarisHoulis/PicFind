3bd1e328d9c825001ac71c0e98cbfbce99bf06f9
houlisha <16525973+HarisHoulis@users.noreply.github.com>
2025-02-15 14:04:23 +0100

    feature: Add foojay-resolver ConventionPlugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,6 +7,10 @@ pluginManagement {
     includeBuild("build-logic")
 }
 
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.9.0"
+}
+
 @Suppress("UnstableApiUsage")
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
